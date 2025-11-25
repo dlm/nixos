d168d6027e8502b3aa74d817c0792034976f1d71
David Millman <dave@cs.unc.edu>
2025-11-25 09:43:14 -0700

    Add ai-tools and codex

--- flake.nix
+++ flake.nix
@@ -4,11 +4,11 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     nix-flatpak.url = "github:gmodena/nix-flatpak";
+    nix-ai-tools.url = "github:numtide/nix-ai-tools";
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-
   };
 
   outputs =
@@ -20,6 +20,9 @@
           ./hosts/default/configuration.nix
           inputs.home-manager.nixosModules.default
           inputs.nix-flatpak.nixosModules.nix-flatpak
+          {
+            home-manager.extraSpecialArgs = { inherit inputs; };
+          }
         ];
       };
     };
--- modules/home/ai.nix
+++ modules/home/ai.nix
@@ -2,17 +2,22 @@
   pkgs,
   lib,
   config,
+  inputs,
   ...
 }:
+let
+  ai-tools = inputs.nix-ai-tools.packages.${pkgs.system};
+in
 {
   options = {
     home.ai.enable = lib.mkEnableOption "enables ai tooling";
   };
 
-  config = lib.mkIf config.home.nushell.enable {
+  config = lib.mkIf config.home.ai.enable {
     home.packages = with pkgs; [
       gemini-cli
       claude-code
+      ai-tools.codex
     ];
   };
 }
