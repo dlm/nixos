079069da1d94bb01a3614b0dc27f4ccb31304c8f
David Millman <dave@cs.unc.edu>
2026-01-27 12:47:28 -0700

    Update to nixos-25.11

--- flake.nix
+++ flake.nix
@@ -2,11 +2,11 @@
   description = "Nixos config flake";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
     nix-flatpak.url = "github:gmodena/nix-flatpak";
     nix-ai-tools.url = "github:numtide/nix-ai-tools";
     home-manager = {
-      url = "github:nix-community/home-manager";
+      url = "github:nix-community/home-manager/release-25.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
