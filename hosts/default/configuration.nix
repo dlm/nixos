18e5f40da12ed651d87f4abf9896f0abcd195583
David Millman <dave@cs.unc.edu>
2026-02-02 09:29:44 -0700

    Fix quoting

--- hosts/default/configuration.nix
+++ hosts/default/configuration.nix
@@ -110,7 +110,7 @@ in
   # Enable touchpad support (enabled default in most desktopManager).
   # services.xserver.libinput.enable = true;
 
-  # Define a user account. Don't forget to set a password with ‘passwd’.
+  # Define a user account. Don't forget to set a password with 'passwd'.
   users.users.dave = {
     isNormalUser = true;
     shell = pkgs.nushell;
