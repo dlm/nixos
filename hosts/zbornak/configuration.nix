e3feb4f649d90885d637dd889702ebd2ba16c953
David Millman <dave@cs.unc.edu>
2026-04-12 21:20:38 -0600

    Fix the display so I can see it

--- hosts/zbornak/configuration.nix
+++ hosts/zbornak/configuration.nix
@@ -13,6 +13,13 @@
 
   networking.hostName = "zbornak";
 
+  # HiDPI display (2880x1920 @ 13") - 1.5x scaling
+  home-manager.users.dave = {
+    xresources.properties = {
+      "Xft.dpi" = "144";
+    };
+  };
+
   # Enable firmware updates (recommended for Framework laptops)
   services.fwupd.enable = true;
 
