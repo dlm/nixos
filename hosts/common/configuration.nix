0e41241bb0fbef218c5732de434cde9699b792bb
David Millman <dave@cs.unc.edu>
2026-04-18 22:52:08 -0600

    Setup ssh

--- hosts/common/configuration.nix
+++ hosts/common/configuration.nix
@@ -95,6 +95,11 @@ in
       "docker"
     ];
     packages = with pkgs; [ ];
+    openssh.authorizedKeys.keys = [
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIH+DG0xr1r6Z/lfhI97nUZvUDhQTiSbxaCyRP7GKfJ52 dave@zbornak"
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJeNhaORfgxVdjqqEX2/rR/4z6viQYVFrGidXx4N9F4V dave@petrillo"
+    ];
+
   };
 
   # Enable removable media management
@@ -207,4 +212,14 @@ in
   services.resolved.enable = true;
 
   services.tailscale.enable = true;
+
+  services.openssh = {
+    enable = true;
+    settings = {
+      PasswordAuthentication = false;
+      KbdInteractiveAuthentication = false;
+      PermitRootLogin = "no";
+      AllowUsers = [ "dave" ];
+    };
+  };
 }
